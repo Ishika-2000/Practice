39254aa1a34a9e3163e04a5a3e443b4109a641f2
Ishika Dubey <ishikadubey92@gmail.com>
2021-12-08 01:55:48 +0530

    first commit

--- final/src/index.js
+++ final/src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import store from './redux/store';
 import { Provider } from 'react-redux';
 
+
+
 ReactDOM.render(
 	<React.StrictMode>
 		<Provider store={store}>
--- final/src/redux/todoSlice.js
+++ final/src/redux/todoSlice.js
@@ -1,5 +1,6 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { nanoid } from 'nanoid';
+import { nanoid } from 'nanoid'; 
+
 
 export const getTodosAsync = createAsyncThunk(
 	'todos/getTodosAsync',
